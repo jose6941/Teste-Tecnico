9d315fded93d3ea546a9b54af71657bce13076d0
Jose6941 <jose.souza1@aluno.ifsp.edu.br>
2025-08-17 10:10:35 -0300

    Código python para scrapy

---
+++ WebScrapy/books/books/spiders/livros.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response):
+        for livro in response.css('.product_pod'):
+            yield{
+                "imagem": livro.css('.thumbnail ::attr(src)').get(),
+                "titulo":  livro.css('.product_pod h3 a ::text').get(),
+                "preco": livro.css('.price_color ::text').get(),
+                "estoque": livro.xpath('//p[@class="instock availability"]/text()[2]').get().strip()
+            }
+            
