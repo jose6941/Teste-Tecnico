246df4731f303a01895b07de4932bd635d4bea4e
Jose6941 <jose.souza1@aluno.ifsp.edu.br>
2025-09-21 15:01:03 -0300

    Raspagem do site quotes

--- WebScrapy/books/books/spiders/livros.py
+++ WebScrapy/books/books/spiders/livros.py
@@ -1,15 +1,17 @@
 import scrapy
 
-class BooksSpider(scrapy.Spider):
-    name = "books"
-    start_urls = ["https://books.toscrape.com/"]
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
-        for livro in response.xpath('//article[@class="product_pod"]'):
-            yield{
-                "imagem": livro.xpath('.//img[@class="thumbnail"]/@src').get(),
-                "titulo":  livro.xpath('.//h3/a/@title').get(),
-                "preco": livro.xpath('.//p[@class="price_color"]/text()').get(),
-                "estoque": livro.xpath('//p[@class="instock availability"]/text()[2]').get().strip()
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                "texto": quote.xpath('.//span[@class="text"]/text()').get(),
+                "autor": quote.xpath('.//small[@class="author"]/text()').get(),
+                "tags": quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall(),
             }
-            
+
+        prox_pagina = response.xpath('//li[@class="next"]/a/@href').get()
+        if prox_pagina:
+            yield response.follow(prox_pagina, callback=self.parse)
